b4e4776d182c9ee4aa82a4aab0971ddbb443eff3
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-28 21:46:23 +0300

    change slider

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,64 @@
-$(".connection__slider").slick({
+$(".tariffs__slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  infinite: true,
+  dots: false,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1440,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+        arrows: false,
       },
     },
     {
       breakpoint: 768,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
+        arrows: false,
+      },
+    },
+  ],
+});
+
+$(".connection__slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  infinite: true,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+      },
+    },
+  ],
+});
+
+$(".reviews__slider").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
       },
     },
   ],
