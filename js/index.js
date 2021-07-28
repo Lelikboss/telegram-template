59980e4dcc9d01de2cddb310045c9ae9f5d5867b
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-28 19:02:57 +0300

    demo slider

--- js/index.js
+++ js/index.js
@@ -12,35 +12,3 @@
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
-
-var slider = Peppermint(document.getElementById("peppermint"), {
-  dots: true,
-  slideshow: true,
-  speed: 500,
-  slideshowInterval: 5000,
-  stopSlideshowAfterInteraction: true,
-  onSetup: function (n) {
-    console.log("Peppermint setup done. Slides found: " + n);
-  },
-});
-
-var connectSlider = Peppermint(document.getElementById("peppermint-connect"), {
-  dots: true,
-  slideshow: true,
-  speed: 500,
-  slideshowInterval: 5000,
-  stopSlideshowAfterInteraction: true,
-  onSetup: function (n) {
-    console.log("Peppermint setup done. Slides found: " + n);
-  },
-});
-var reviewsSlider = Peppermint(document.getElementById("peppermint-reviews"), {
-  dots: true,
-  slideshow: true,
-  speed: 500,
-  slideshowInterval: 5000,
-  stopSlideshowAfterInteraction: true,
-  onSetup: function (n) {
-    console.log("Peppermint setup done. Slides found: " + n);
-  },
-});
---
+++ js/slider.js
@@ -0,0 +1,21 @@
+$(".connection__slider").slick({
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+  ],
+});
