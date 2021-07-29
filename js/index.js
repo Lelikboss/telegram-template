cb4fc56cebc444d227451a16f7fcdb31dc21086d
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-29 20:46:49 +0300

    add Click on enter btn

--- js/index.js
+++ js/index.js
@@ -12,3 +12,19 @@
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+const enterBtn = document.querySelector(".enter--btn"); //! кнопка входа
+const tabletEnterBtn = document.querySelector(".tablet-enter--btn");
+const authorBlockEl = document.querySelector(".header-author--btn");
+const mobileAuthorBlockEl = document.querySelector(".mobile-author--block");
+const mobileMenuEl = document.querySelector(".mobile--menu"); //! menu
+const headerNavEl = document.querySelector(".header--nav"); //! menu
+
+enterBtn.addEventListener("click", clickOnEnterBtn);
+tabletEnterBtn.addEventListener("click", clickOnEnterBtn);
+function clickOnEnterBtn() {
+  mobileMenuEl.classList.remove("inactive--menu");
+  headerNavEl.classList.remove("inactive--menu");
+  authorBlockEl.classList.add("inactive--menu");
+  mobileAuthorBlockEl.classList.add("inactive--menu");
+}
